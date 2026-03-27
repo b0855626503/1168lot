1e4dc40206d3042bb51ef9bc9fa1fdd852b99dad
Boat <b0855626503@gmail.com>
2026-03-27 10:15:35 +0700

    อัพเดท เพิ่มคลาส `btn-block` ให้ปุ่มใน datatables_actions เพื่อปรับปรุงการจัดเรียงและการแสดงผล

--- packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/draws/datatables_actions.blade.php
+++ packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/draws/datatables_actions.blade.php
@@ -1,29 +1,29 @@
 @if($status !== 'resulted')
     <div class="d-flex flex-wrap justify-content-center">
-        <button type="button" class="btn btn-info btn-xs mr-1 mb-1" onclick="editModal({{ $id }})">
+        <button type="button" class="btn btn-info btn-xs btn-block mr-1 mb-1" onclick="editModal({{ $id }})">
             <i class="fas fa-edit"></i> แก้ไข
         </button>
         @if($status === 'draft' || $status === 'closed')
-            <button type="button" class="btn btn-primary btn-xs mr-1 mb-1" onclick="openDraw({{ $id }})">
+            <button type="button" class="btn btn-primary btn-xs btn-block mr-1 mb-1" onclick="openDraw({{ $id }})">
                 <i class="fas fa-play"></i> เปิดรับ
             </button>
         @endif
         @if($status === 'open')
-            <button type="button" class="btn btn-secondary btn-xs mr-1 mb-1" onclick="closeDraw({{ $id }})">
+            <button type="button" class="btn btn-secondary btn-xs btn-block mr-1 mb-1" onclick="closeDraw({{ $id }})">
                 <i class="fas fa-stop"></i> ปิดรับ
             </button>
         @endif
         @if($status === 'closed')
-            <button type="button" class="btn btn-success btn-xs mr-1 mb-1" onclick="settleModal({{ $id }})">
+            <button type="button" class="btn btn-success btn-xs btn-block mr-1 mb-1" onclick="settleModal({{ $id }})">
                 <i class="fas fa-check-circle"></i> ประกาศผล
             </button>
-            <button type="button" class="btn btn-warning btn-xs mr-1 mb-1" onclick="runAutoResultTestFetch({{ $id }})">
+            <button type="button" class="btn btn-warning btn-xs btn-block mr-1 mb-1" onclick="runAutoResultTestFetch({{ $id }})">
                 <i class="fas fa-vial"></i> Dry-run
             </button>
-            <button type="button" class="btn btn-dark btn-xs mr-1 mb-1" onclick="runAutoResultManualRetry({{ $id }})">
+            <button type="button" class="btn btn-dark btn-xs btn-block mr-1 mb-1" onclick="runAutoResultManualRetry({{ $id }})">
                 <i class="fas fa-redo"></i> Retry
             </button>
-            <button type="button" class="btn btn-secondary btn-xs mr-1 mb-1" onclick="showAutoResultLogs({{ $id }})">
+            <button type="button" class="btn btn-secondary btn-xs btn-block mr-1 mb-1" onclick="showAutoResultLogs({{ $id }})">
                 <i class="fas fa-stream"></i> Logs
             </button>
         @endif
