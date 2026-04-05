b04dad4114d4ef67bc68e6acaa42d0e23f9e7e8a
Boat <b0855626503@gmail.com>
2026-04-06 06:23:55 +0700

    Fix client-only Reverb connection config for 1168lot

--- resources/views/partials/broadcast-config.blade.php
+++ resources/views/partials/broadcast-config.blade.php
@@ -1,9 +1,9 @@
 <script>
     window.broadcastConfig = {
-        key: @json(config('reverb.apps.apps.0.key', config('broadcasting.connections.reverb.key', 'app-key'))),
-        host: @json(config('reverb.apps.apps.0.options.host', request()->getHost())),
-        port: @json((int) config('reverb.apps.apps.0.options.port', config('broadcasting.connections.reverb.options.port', 8080))),
-        scheme: @json(config('reverb.apps.apps.0.options.scheme', request()->isSecure() ? 'https' : 'http')),
-        path: @json(config('reverb.servers.reverb.path', '')),
+        key: @json(config('broadcasting.connections.reverb.key', config('broadcasting.connections.pusher.key', 'app-key'))),
+        host: @json(config('broadcasting.connections.reverb.options.host', config('broadcasting.connections.pusher.options.host', request()->getHost()))),
+        port: @json((int) config('broadcasting.connections.reverb.options.port', config('broadcasting.connections.pusher.options.port', 443))),
+        scheme: @json(config('broadcasting.connections.reverb.options.scheme', config('broadcasting.connections.pusher.options.scheme', request()->isSecure() ? 'https' : 'http'))),
+        path: @json(config('broadcasting.connections.reverb.options.path', '')),
     };
 </script>
