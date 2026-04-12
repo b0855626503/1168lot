157eb012bfc5edb06ff2b71b55a1243ea686290a
Boat <b0855626503@gmail.com>
2026-04-13 01:27:05 +0700

    Add game folder

---
+++ game/game.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'seamless' => [
+        'apiurl' => env('APIURL', 'https://test.ambsuperapi.com/'),
+        'agent' => env('UsernameAPI', ''),
+        'secretkey' => env('PasswordAPI', ''),
+        's' => env('s', '0365abe6-7b12-4d25-bfa8-ede19d7a9cfe')
+    ],
+];
+
