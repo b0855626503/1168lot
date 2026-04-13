cb0881e9df0c6b0ac22f33c19ce9afadd5c88e45
b0855626503 <b0855626503@gmail.com>
2026-04-13 18:15:48 +0700

    Remove legacy game configuration files and update .gitignore

--- game/gamefree.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'seamless' => [
-        'apiurl' => env('APIURL', 'https://api.hentory.io/'),
-        'agent' => env('UsernameAPI', ''),
-        'secretkey' => env('PasswordAPI', '7765e230-5a5a-4cd3-854c-faca6bd52a97')
-    ],
-];
