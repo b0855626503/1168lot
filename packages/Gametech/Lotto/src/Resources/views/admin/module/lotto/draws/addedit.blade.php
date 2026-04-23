8cddc61698015f1d01efd084109bcebfeacfe571
b0855626503 <b0855626503@gmail.com>
2026-04-23 23:08:51 +0700

    fix(lotto): restore datetime picker assets for draws form

--- packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/draws/addedit.blade.php
+++ packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/draws/addedit.blade.php
@@ -702,11 +702,14 @@
             white-space: nowrap;
         }
     </style>
+    <link rel="stylesheet" href="{{ asset('vendor/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/select2/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
 @endpush
 
 @push('scripts')
+    <script src="{{ asset('vendor/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('vendor/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
     <script src="{{ asset('vendor/select2/js/select2.full.min.js') }}"></script>
 
     <script type="module">
@@ -1358,7 +1361,6 @@
                         }
 
                         $input.prop('readonly', disabled);
-                        $input.prop('disabled', disabled);
 
                         if (typeof $input.datetimepicker === 'function' && $input.data('datetimepicker')) {
                             if (disabled) {
