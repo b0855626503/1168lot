4d47a14c24e37469e011c90f6afac64f5f2636a4
b0855626503 <b0855626503@gmail.com>
2026-04-23 17:35:46 +0700

    fix(admin): update `WithdrawController` to set bill completion status as 'N' instead of 'R'

--- packages/Gametech/Auto/src/Jobs/PaymentKbank.php
+++ packages/Gametech/Auto/src/Jobs/PaymentKbank.php
@@ -139,7 +139,12 @@ class PaymentKbank implements ShouldQueue, ShouldBeUnique
                 if (!$txDate) {
                     continue;
                 }
-                if (core()->DateDiff($txDate) > 1) { // คงเงื่อนไขเดิม
+//                if (core()->DateDiff($txDate) > 1) { // คงเงื่อนไขเดิม
+//                    continue;
+//                }
+                $diff = core()->DateDiffMin($txDate);
+
+                if ($diff < 0 || $diff > 20) {
                     continue;
                 }
 
