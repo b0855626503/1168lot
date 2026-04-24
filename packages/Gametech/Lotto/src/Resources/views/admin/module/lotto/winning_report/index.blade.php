ac68d912b243eecc78036ee1c778f24513431513
b0855626503 <b0855626503@gmail.com>
2026-04-24 19:02:30 +0700

    fix(lotto): update label and placeholder text for user filter in winning reports

--- packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/winning_report/index.blade.php
+++ packages/Gametech/Lotto/src/Resources/views/admin/module/lotto/winning_report/index.blade.php
@@ -465,8 +465,8 @@
                             </div>
                             <div class="col-lg-3 col-md-3 col-sm-6 mb-2">
                                 <div class="wr-filter-block">
-                                    <div class="wr-filter-label">รหัสสมาชิก (ไอดีลูกค้า)</div>
-                                    <input v-model.trim="detailFilters.user_id" type="text" class="form-control form-control-sm" placeholder="เช่น 12345 หรือ boat001" @keyup.enter.prevent="loadDetails">
+                                    <div class="wr-filter-label">Username สมาชิก</div>
+                                    <input v-model.trim="detailFilters.user_id" type="text" class="form-control form-control-sm" placeholder="เช่น boat001" @keyup.enter.prevent="loadDetails">
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-3 col-sm-6 mb-2">
