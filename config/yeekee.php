296b93868666c2e480e5475af0b822a407072515
1168lot Dev <dev@1168lot.local>
2026-05-07 21:17:44 +0700

    [BOA-233]

--- config/yeekee.php
+++ config/yeekee.php
@@ -6,7 +6,7 @@ return [
     'shoot_enabled' => (bool) env('YEEKEE_SHOOT_ENABLED', false),
     'shooting_enabled' => (bool) env('YEEKEE_SHOOTING_ENABLED', true),
     'result_enabled' => (bool) env('YEEKEE_RESULT_ENABLED', false),
-    'reward_enabled' => (bool) env('YEEKEE_REWARD_ENABLED', false),
+    'reward_enabled' => (bool) env('YEEKEE_REWARD_ENABLED', true),
     'settlement_enabled' => (bool) env('YEEKEE_SETTLEMENT_ENABLED', false),
     'max_shoots_per_member_per_round' => (int) env('YEEKEE_MAX_SHOOTS_PER_MEMBER_PER_ROUND', 100),
     'max_shoots_per_ip_per_minute' => (int) env('YEEKEE_MAX_SHOOTS_PER_IP_PER_MINUTE', 30),
